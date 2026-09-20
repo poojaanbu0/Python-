33e2284765bbc1ff29f762183aebbea398410f1d
poojaanbu0 <poojaanbu0@gmail.com>
2026-09-20 21:03:02 +0530

    practice 1

--- coding_practice/freq_count_Dict.py
+++ coding_practice/freq_count_Dict.py
@@ -1,7 +1,32 @@
-a = [1,2,3,3,2,3,24,4]
+# def frequency(a):
+#     freq = {}
 
-freq = {}
+#     for num in a:
+#         freq[num] = freq.get(num,0) + 1
+#     print(freq)
 
-for num in a:
-    freq[num] = freq.get(num,0) + 1
-print(freq)
+# s = list(map(int, input("enter array").split()))
+# frequency(s)
+
+# s = ['1', '2', '3', '4']
+# res = map(int, s)
+# print(res)
+# print(next(res))
+# print(next(res))
+# print(next(res))
+# print(list(res))
+# print(list(res))
+
+def palindrome(str):
+    x = ""
+    for char in str:
+        x = char + x
+
+    if (x==str):
+        print("palindrome")
+    else:
+        print("not palindrome")
+
+str = input("Enter str")
+
+palindrome(str)
--- coding_practice/reverse_string.py
+++ coding_practice/reverse_string.py
@@ -1,6 +1,8 @@
-str = input("enter string")
-reversed_str = ""
+def reverse_string(str):
+    reversed_str = ""
+    for char in str:
+        reversed_str = char + reversed_str
+    print(reversed_str)
 
-for char in str:
-    reversed_str = char + reversed_str
-print(reversed_str)
+str = input("enter string")
+reverse_string(str)
