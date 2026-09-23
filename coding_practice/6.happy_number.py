c53b0cf9adaf10df58ba2893d9791780825540a2
poojaanbu0 <poojaanbu0@gmail.com>
2026-09-23 08:51:43 +0530

    practice

--- coding_practice/6.happy_number.py
+++ coding_practice/6.happy_number.py
@@ -1,2 +1,16 @@
 # happy number = sum of its square of its digits
 # 19 = sq(1) + sq(9) = 1 + 81 = 82 => sq(2) + sq(8) = 4 + 64 = 68 => 36 + 64 = > 100 => 1 + 0 + 0 = 1
+import math
+def happy_number(n):
+    
+    while sq != 1:
+        x = n % 10
+        sq_x = math.pow(x, 2)
+        y = n // 10
+        sq_y = math.pow(y, 2)
+        sq=int(sq_x) + int(sq_y)
+        print(sq)
+    
+
+n = int(input("enter a num "))
+happy_number(n)
